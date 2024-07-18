9acdc3d451ae1e9d000dd451cc853972c375802d
01124833532mo <mohammedhamdi726@gmail.com>
2024-07-19 01:45:23 +0300

    class og problem1

--- Assement/3Dpoint.cs
+++ Assement/3Dpoint.cs
@@ -6,27 +6,28 @@ using System.Threading.Tasks;
 
 namespace Assement
 {
-    internal class _3Dpoint :IComparable, ICloneable
+    internal class _3Dpoint : IComparable, ICloneable
     {
         public int X { get; set; }
         public int Y { get; set; }
-        public int Z { get; set; } 
+        public int Z { get; set; }
 
-        public _3Dpoint(int _x) {
-        X= _x;
+        public _3Dpoint(int _x)
+        {
+            X = _x;
             Y = 0;
             Z = 0;
-        
+
         }
 
-        public _3Dpoint(int _x,int _y) : this(_x) 
+        public _3Dpoint(int _x, int _y) : this(_x)
         {
             Y = _y;
             Z = 0;
         }
-        public _3Dpoint(int _x, int _y,int _z) : this(_x,  _y)
+        public _3Dpoint(int _x, int _y, int _z) : this(_x, _y)
         {
-            Z = _z;  
+            Z = _z;
         }
 
 
@@ -40,7 +41,7 @@ namespace Assement
             _3Dpoint? Emp = (_3Dpoint?)obj;
 
             int comparx = this.X.CompareTo(Emp?.X);
-            if(comparx != 0)
+            if (comparx != 0)
                 return comparx;
 
             int compary = this.Y.CompareTo(Emp?.Y);
@@ -53,10 +54,11 @@ namespace Assement
         public object Clone()
         {
             return new _3Dpoint(this.X, this.Y, this.Z); // because we have chining constractor
-           
+
         }
 
-        public static bool operator == (_3Dpoint left, _3Dpoint right) {
+        public static bool operator ==(_3Dpoint left, _3Dpoint right)
+        {
 
             if ((left?.X ?? 0) == (right?.X ?? 0) && (left?.Y ?? 0) == (right?.Y ?? 0) && (left?.Z ?? 0) == (right?.Z ?? 0))
             {
